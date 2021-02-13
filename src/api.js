9a31997bdafde7a28962e8b94e987eea8ea5437d
Ashley <73862628+ash-lowe@users.noreply.github.com>
2021-02-13 18:38:30 -0500

    fixing api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,33 @@
 var UserAccount = require('./useraccount');
 const dboperations = require('./dboperations');
 
+
+var express = require('express');
+var bodyParser = require('body-parser')
+var cors = require('cors');
+var app = express();
+var router = exress.Router();
+
+app.use(bodyParser.urlencoded({extended: truth}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api',router);
+
+router.use((request,response,next)=>{
+    console.log('middleware');
+    next();
+})
+
+router.route('/useraccount').get((request,response)=>{
+    dboperations.getUsers().then(result =>{
+        response.json(result[0])
+    })
+})
+
+var port = process.env.PORT || 5140;
+app.listen(port);
+console.log('Order API is running at' + port);
+
 dboperations.getUsers().then(result =>{
     console.log(result);
 })
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import  usersRoutes from './routes/users.js';
 
 const app = express();
-const PORT = 5000;
+const PORT = 5140;
 
 app.use(bodyParser.json());
 
